06b834d80e4995ce922bb99ecbaf10df8b7506c8
Gabriel Horner <gabriel.horner@gmail.com>
2009-06-09 20:49:43 -0400

    basic boson tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'test/unit'
 require 'context' #gem install jeremymcanally-context --source http://gems.github.com
 require 'matchy' #gem install jeremymcanally-matchy --source http://gems.github.com
+require 'mocha'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'boson'
 
