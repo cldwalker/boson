ee6702cc6f2d959de5565169f6100fb2c83f0c1e
Gabriel Horner <gabriel.horner@gmail.com>
2009-08-27 07:17:09 -0400

    some web related core commands

--- lib/boson/commands/core.rb
+++ lib/boson/commands/core.rb
@@ -26,6 +26,30 @@ module Boson
       def menu(output, options={}, &block)
         ::Hirb::Console.format_output(output, options.merge(:class=>"Hirb::Menu"), &block)
       end
+
+      def get(url)
+        require 'net/http'
+        Net::HTTP.get(URI.parse(url))
+      end
+
+      def download(url)
+        require 'fileutils'
+        FileUtils.mkdir_p(File.join(Boson.dir,'downloads'))
+        response = get(url)
+        filename = determine_download_name(url)
+        File.open(filename, 'w') { |f| f.write response }
+        filename
+      end
+
+      private
+      def determine_download_name(url)
+        require 'uri'
+        basename = URI.parse(url).path.split('/')[-1]
+        basename = URI.parse(url).host.sub('www.','') if basename.nil? || basename.empty?
+        filename = File.join(Boson.dir, 'downloads', basename)
+        filename += "-#{Time.now.strftime("%m_%d_%y_%H_%M_%S")}" if File.exists?(filename)
+        filename
+      end
     end
   end
 end
