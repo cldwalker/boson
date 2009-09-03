7329a72eb9a9c973d1e8b02feaee17c58d5d4044
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-03 18:28:30 -0400

    cleaner safeguarding of core commands

--- lib/boson/commands/core.rb
+++ lib/boson/commands/core.rb
@@ -1,46 +1,43 @@
-module Boson
-  module Commands
-    module Core
-      def commands(query='', options={})
-        options = {:fields=>[:name, :lib, :alias],:search_field=>:name}.merge(options)
-        search_field = options.delete(:search_field)
-        results = ::Boson.commands.select {|f| f.send(search_field) =~ /#{query}/ }
-        render results, options
-      end
+module Boson::Commands; end # avoid having to :: prefix all classes
+module Boson::Commands::Core
+  def commands(query='', options={})
+    options = {:fields=>[:name, :lib, :alias],:search_field=>:name}.merge(options)
+    search_field = options.delete(:search_field)
+    results = Boson.commands.select {|f| f.send(search_field) =~ /#{query}/ }
+    render results, options
+  end
 
-      def libraries(query='', options={})
-        options = {:fields=>[:name, :commands, :gems, :library_type], :search_field=>:name,
-          :filters=>{:gems=>lambda {|e| e.join(',')},:commands=>:size}}.merge(options)
-        search_field = options.delete(:search_field)
-        results = ::Boson.libraries.select {|f| f.send(search_field) =~ /#{query}/ }
-        render results, options
-      end
+  def libraries(query='', options={})
+    options = {:fields=>[:name, :commands, :gems, :library_type], :search_field=>:name,
+      :filters=>{:gems=>lambda {|e| e.join(',')},:commands=>:size}}.merge(options)
+    search_field = options.delete(:search_field)
+    results = Boson.libraries.select {|f| f.send(search_field) =~ /#{query}/ }
+    render results, options
+  end
 
-      def unloaded_libraries
-        (::Boson::Runner.all_libraries - ::Boson.libraries.map {|e| e.name }).sort
-      end
+  def unloaded_libraries
+    (Boson::Runner.all_libraries - Boson.libraries.map {|e| e.name }).sort
+  end
 
-      def load_library(library, options={})
-        ::Boson::Library.load_library(library, {:verbose=>true}.merge!(options))
-      end
+  def load_library(library, options={})
+    Boson::Library.load_library(library, {:verbose=>true}.merge!(options))
+  end
 
-      def reload_library(name, options={})
-        ::Boson::Library.reload_library(name, {:verbose=>true}.merge!(options))
-      end
+  def reload_library(name, options={})
+    Boson::Library.reload_library(name, {:verbose=>true}.merge!(options))
+  end
 
-      def index
-        ::Boson::Runner.index_commands
-        puts "Indexed #{::Boson.libraries.size} libraries and #{::Boson.commands.size} commands."
-      end
+  def index
+    Boson::Runner.index_commands
+    puts "Indexed #{Boson.libraries.size} libraries and #{Boson.commands.size} commands."
+  end
 
-      def render(object, options={})
-        options[:class] = options.delete(:as) || :auto_table
-        ::Hirb::Console.render_output(object, options)
-      end
+  def render(object, options={})
+    options[:class] = options.delete(:as) || :auto_table
+    Hirb::Console.render_output(object, options)
+  end
 
-      def menu(output, options={}, &block)
-        ::Hirb::Console.format_output(output, options.merge(:class=>"Hirb::Menu"), &block)
-      end
-    end
+  def menu(output, options={}, &block)
+    Hirb::Console.format_output(output, options.merge(:class=>"Hirb::Menu"), &block)
   end
 end
--- lib/boson/commands/namespace.rb
+++ lib/boson/commands/namespace.rb
@@ -1,24 +1,20 @@
-module Boson
-  module Commands
-    module Namespace
-      def self.create(name, lib_module)
-        module_eval %[
-          def #{name}
-            @#{name} ||= begin
-              obj = ::Object.new
-              obj.instance_eval("class<<self;self;end").send :include, ::#{lib_module}
-              def obj.commands
-                ::#{lib_module}.instance_methods
-              end
-              # private
-              def obj.method_missing(method, *args, &block)
-                ::Boson.invoke(method, *args, &block)
-              end
-              obj
-            end
+module Boson::Commands::Namespace
+  def self.create(name, lib_module)
+    module_eval %[
+      def #{name}
+        @#{name} ||= begin
+          obj = Object.new
+          obj.instance_eval("class<<self;self;end").send :include, #{lib_module}
+          def obj.commands
+            #{lib_module}.instance_methods
           end
-        ]
+          # private
+          def obj.method_missing(method, *args, &block)
+            Boson.invoke(method, *args, &block)
+          end
+          obj
+        end
       end
-    end
+    ]
   end
 end
