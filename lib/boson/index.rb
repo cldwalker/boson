4d55f233808cf752e8d8df2cedbdc88d0d4bf32b
Gabriel Horner <gabriel.horner@gmail.com>
2009-09-21 18:29:17 -0400

    partially working incremental index

--- lib/boson/index.rb
+++ lib/boson/index.rb
@@ -1,3 +1,4 @@
+require 'digest/md5'
 module Boson
   module Index
     extend self
@@ -10,16 +11,19 @@ module Boson
       File.open(marshal_file, 'w') {|f| f.write marshal_string }
     end
 
-    def marshal_read
+    def marshal_read(ignored_libraries=[])
       new_libraries, new_commands = Marshal.load(File.read(marshal_file))
-      existing_libraries = Boson.libraries.map {|e| e.name}
+      existing_libraries = (Boson.libraries.map {|e| e.name} + ignored_libraries).uniq
       Boson.libraries += new_libraries.select {|e| !existing_libraries.include?(e.name)}
       existing_commands = Boson.commands.map {|e| e.name}
       Boson.commands += new_commands.select {|e| !existing_commands.include?(e.name)}
     end
 
     def index_commands(options={})
-      Library.load(Runner.all_libraries, options.merge(:index=>true))
+      index_libraries = options[:index_all] ? Runner.all_libraries : changed_libraries
+      marshal_read(index_libraries)
+      Library.load(index_libraries, options.merge(:index=>true))
+      write_hashes
       marshal_write
     end
 
@@ -44,6 +48,27 @@ module Boson
       else
         marshal_read
       end
+    end
+
+    def write_hashes
+      File.open(hash_file, 'w') {|f| f.write(latest_hashes.to_yaml)}
+    end
+
+    def changed_libraries
+      old_hashes = YAML::load_file hash_file rescue {}
+      latest_hashes.select {|lib, hash| old_hashes[lib] != hash}.map {|e| e[0]}
+    end
+
+    def hash_file
+      hash_file = File.join(Boson.repo.config_dir, "library_hashes.yml")
+    end
+
+    def latest_hashes
+      Boson::Runner.all_libraries.inject({}) {|h, e|
+        lib_file = Boson::FileLibrary.library_file(e, Boson.repo.dir)
+        h[e] = Digest::MD5.hexdigest(File.read(lib_file)) if File.exists?(lib_file)
+        h
+      }
     end
   end
 end
