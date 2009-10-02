722ae08eb626f5a63e126bd8f4ca43cee7e52609
Gabriel Horner <gabriel.horner@gmail.com>
2009-10-02 18:07:05 -0400

    fixed test creating local config directory

--- test/repo_test.rb
+++ test/repo_test.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class Boson::RepoTest < Test::Unit::TestCase
   context "config" do
     before(:all) { reset }
-    before(:each) { @repo = Boson::Repo.new('.') }
+    before(:each) { @repo = Boson::Repo.new(File.dirname(__FILE__)) }
 
     test "reloads config when passed true" do
       @repo.config.object_id.should_not == @repo.config(true).object_id
