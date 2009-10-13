dd62c117f8cccd6f5c137df6263546a47181bda4
Gabriel Horner <gabriel.horner@gmail.com>
2009-10-13 18:17:26 -0400

    improved install to wrap methods or just ruby code

--- lib/boson/commands/web_core.rb
+++ lib/boson/commands/web_core.rb
@@ -2,9 +2,12 @@ module Boson::Commands::WebCore #:nodoc:
   def self.config
     descriptions = {
       :install=>"Installs a library by url. Library should then be loaded with load_library.",
-      :browser=>"Opens urls in a browser", :get=>"Gets the body of a url" }
+      :browser=>"Opens urls in a browser on a Mac", :get=>"Gets the body of a url" }
     commands = descriptions.inject({}) {|h,(k,v)| h[k.to_s] = {:description=>v}; h}
-    commands['install'][:options] = {:name=>:string, :force=>:boolean}
+    commands['install'][:options] = {:name=>{:type=>:string, :desc=>"Library name to save to"},
+      :force=>{:type=>:boolean, :desc=>'Overwrites an existing library'},
+      :module_wrap=>{:type=>:boolean, :desc=>"Wraps a module around install using library name"},
+      :method_wrap=>{:type=>:boolean, :desc=>"Wraps a method and module around installe library using library name"}}
     {:library_file=>File.expand_path(__FILE__), :commands=>commands}
   end
 
@@ -17,10 +20,20 @@ module Boson::Commands::WebCore #:nodoc:
 
   def install(url, options={})
     options[:name] ||= strip_name_from_url(url)
-    return puts("Please give a library name with this url.") unless options[:name]
+    return puts("Please give a library name for this url.") if options[:name].empty?
     filename = File.join Boson.repo.commands_dir, "#{options[:name]}.rb"
     return puts("Library name #{options[:name]} already exists. Try a different name.") if File.exists?(filename) && !options[:force]
     File.open(filename, 'w') {|f| f.write get(url) }
+
+    if options[:method_wrap] || options[:module_wrap]
+      file_string = File.read(filename)
+      file_string = "def #{options[:name]}\n#{file_string}\nend" if options[:method_wrap]
+      unless (mod_name = Boson::Util.camelize(options[:name]))
+        return puts("Can't wrap install with name #{options[:name]}")
+      end
+      file_string = "module #{mod_name}\n#{file_string}\nend"
+      File.open(filename, 'w') {|f| f.write file_string }
+    end
     puts "Saved to #{filename}."
   end
 
@@ -31,6 +44,6 @@ module Boson::Commands::WebCore #:nodoc:
 
   private
   def strip_name_from_url(url)
-    url[/\/([^\/.]+)(\.[a-z]+)?$/, 1]
+    url[/\/([^\/.]+)(\.[a-z]+)?$/, 1].to_s.gsub('-', '_').gsub(/[^a-zA-Z_]/, '')
   end
 end
