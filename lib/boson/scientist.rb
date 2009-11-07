7ae6fe2e0df2afde4ea6c802830ced1bc54d81f0
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-07 03:52:43 -0500

    cmds w/ only render opts should still parse global opts

--- lib/boson/scientist.rb
+++ lib/boson/scientist.rb
@@ -270,8 +270,8 @@ module Boson
         parsed_options, new_args = parse_options args
         @args += new_args
       # add default options
-      elsif (!@command.has_splat_args? && @args.size <= (@command.arg_size - 1).abs) ||
-        (@command.has_splat_args? && !@args[-1].is_a?(Hash))
+      elsif @command.options.empty? || (!@command.has_splat_args? &&
+        @args.size <= (@command.arg_size - 1).abs) || (@command.has_splat_args? && !@args[-1].is_a?(Hash))
           parsed_options = parse_options([])[0]
       # merge default options with given hash of options
       elsif (@command.has_splat_args? || (@args.size == @command.arg_size)) && @args[-1].is_a?(Hash)
