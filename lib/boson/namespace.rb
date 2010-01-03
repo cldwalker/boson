42dcd315efd63831c39704a922420fc624ea608e
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-03 14:31:27 -0500

    extracted rarely used + undocumented object_namespace out into a plugin

--- lib/boson/namespace.rb
+++ lib/boson/namespace.rb
@@ -8,24 +8,6 @@ module Boson
 
     # Creates a namespace given its name and the library it belongs to.
     def self.create(name, library)
-      if library.object_namespace && library.module.instance_methods.map {|e| e.to_s}.include?(name)
-        library.include_in_universe
-        create_object_namespace(name, library)
-      else
-        create_basic_namespace(name, library)
-      end
-    end
-    #:stopdoc:
-
-    def self.create_object_namespace(name, library)
-      obj = library.namespace_object
-      obj.instance_eval("class<<self;self;end").send(:define_method, :boson_commands) {
-        self.class.instance_methods(false) }
-      obj.instance_eval("class<<self;self;end").send(:define_method, :object_delegate?) { true }
-      namespaces[name.to_s] = obj
-    end
-
-    def self.create_basic_namespace(name, library)
       namespaces[name.to_s] = new(name, library)
       Commands::Namespace.send(:define_method, name) { Boson::Namespace.namespaces[name.to_s] }
     end
@@ -36,11 +18,10 @@ module Boson
       class <<self; self end.send :include, @library.module
     end
 
-    def object_delegate?; false; end
-
     def method_missing(method, *args, &block)
       Boson.can_invoke?(method) ? Boson.invoke(method, *args, &block) : super
     end
+
     #:startdoc:
     # List of subcommands for the namespace.
     def boson_commands
