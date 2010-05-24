eb7c50bb64f7c788c6472c9e91f744404a2a20f4
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-24 04:36:17 -0400

    file_library test fixes for 1.9.2

--- test/file_library_test.rb
+++ test/file_library_test.rb
@@ -2,6 +2,7 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 
 describe "file library" do
   before { reset; FileLibrary.reset_file_cache }
+  before { Gem.stubs(:loaded_specs).returns({}) } if RUBY_VERSION >= '1.9.2'
 
   it "loads" do
     load :blah, :file_string=>"module Blah; def blah; end; end"
@@ -22,7 +23,8 @@ describe "file library" do
   end
 
   it "loads by basename" do
-    Dir.stubs(:[]).returns(['./test/commands/site/github.rb'])
+    Dir.stubs(:[]).returns([RUBY_VERSION < '1.9.2' ? './test/commands/site/github.rb' :
+      File.expand_path('./test/commands/site/github.rb')])
     load 'github', :file_string=>"module Github; def blah; end; end", :exists=>false
     library_has_module('site/github', "Boson::Commands::Site::Github")
     command_exists?('blah')
