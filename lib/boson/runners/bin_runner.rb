424c6af3f4adac7650836a5dc63f8163a5302ae2
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-24 14:46:39 -0400

    version tweak

--- lib/boson/runners/bin_runner.rb
+++ lib/boson/runners/bin_runner.rb
@@ -56,7 +56,7 @@ module Boson
       # Starts, processes and ends a commandline request.
       def start(args=ARGV)
         @command, @options, @args = parse_args(args)
-        return puts(Boson::VERSION) if @options[:version]
+        return puts("boson #{Boson::VERSION}") if @options[:version]
         return print_usage if args.empty? || (@command.nil? && !@options[:console] && !@options[:execute])
         return ConsoleRunner.bin_start(@options[:console], @options[:load]) if @options[:console]
         init
