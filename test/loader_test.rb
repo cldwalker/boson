8fa475826a428dfecabd5d3b38863a95103106ae
Gabriel Horner <gabriel.horner@gmail.com>
2012-01-24 18:55:11 -0500

    move old loader tests to proper plugin tests

--- test/loader_test.rb
+++ test/loader_test.rb
@@ -101,67 +101,6 @@ describe "Loader" do
         }.should =~ /Unable to load library chwhat.*conflict.*chwhat/
       end
     end
-
-    describe "module library" do
-      def mock_library(*args); end
-
-      it "loads a module library and all its class methods by default" do
-        eval %[module ::Harvey; def self.bird; end; def self.eagle; end; end]
-        load ::Harvey, :no_mock=>true
-        library_has_command('harvey', 'bird')
-        library_has_command('harvey', 'eagle')
-      end
-
-      it "loads a module library with specified commands" do
-        eval %[module ::Peanut; def self.bird; end; def self.eagle; end; end]
-        load ::Peanut, :no_mock=>true, :commands=>%w{bird}
-        library('peanut').commands.size.should == 1
-        library_has_command('peanut', 'bird')
-      end
-
-      it "loads a module library as a class" do
-        eval %[class ::Mentok; def self.bird; end; def self.eagle; end; end]
-        load ::Mentok, :no_mock=>true, :commands=>%w{bird}
-        library('mentok').commands.size.should == 1
-        library_has_command('mentok', 'bird')
-      end
-    end
-
-    describe "gem library" do
-      def mock_library(lib, options={})
-        options[:file_string] ||= ''
-        File.stubs(:exists?).returns(false)
-        GemLibrary.expects(:is_a_gem?).returns(true)
-        Util.expects(:safe_require).with { eval options.delete(:file_string) || ''; true}.returns(true)
-      end
-
-      it "loads" do
-        with_config(:libraries=>{"dude"=>{:module=>'Dude'}}) do
-          load "dude", :file_string=>"module ::Dude; def blah; end; end"
-          library_has_module('dude', "Dude")
-          command_exists?("blah")
-        end
-      end
-
-      it "with kernel methods loads" do
-        load "dude", :file_string=>"module ::Kernel; def dude; end; end"
-        library_loaded? 'dude'
-        library('dude').module.should == nil
-        command_exists?("dude")
-      end
-
-      it "prints error when nonexistent" do
-        capture_stderr { load('blah') }.should =~ /Library blah did not/
-      end
-
-      it "with invalid module prints error" do
-        with_config(:libraries=>{"coolio"=>{:module=>"Cool"}}) do
-          capture_stderr {
-            load('coolio', :file_string=>"module ::Coolio; def coolio; end; end")
-          }.should =~ /Unable.*coolio.*No module/
-        end
-      end
-    end
   end
 
   after_all { FileUtils.rm_r File.dirname(__FILE__)+'/commands/', :force=>true }
