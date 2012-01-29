6495024011bf6be22d610ba676311046763753d2
Gabriel Horner <gabriel.horner@gmail.com>
2012-01-29 15:02:09 -0500

    more loader cleanup

--- lib/boson/loader.rb
+++ lib/boson/loader.rb
@@ -1,25 +1,20 @@
 module Boson
-  # Raised if a library has a method which conflicts with existing methods in Boson.main_object.
+  # Raised if a library has methods which conflict with existing methods
   class MethodConflictError < LoaderError; end
 
-  # This module is mixed into Library to give it load() functionality.
-  # When creating your own Library subclass, you should override load_source_and_set_module
-  # You can override other methods in this module as needed.
+  # This module is mixed into Library to give it load() functionality. When
+  # creating your own Library subclass, you should at least override
+  # load_source_and_set_module.
   module Loader
-    # Loads a library and its dependencies and returns true if library loads correctly.
+    # Loads a library and its dependencies and returns true if library loads
+    # correctly.
     def load
       load_source_and_set_module
       module_callbacks if @module
-      yield if block_given?
-      detect_additions { load_module_commands } if load_module_commands?
-      before_library_commands
+      yield if block_given? # load dependencies
+      detect_additions { initialize_library_module } if load_module_commands?
       set_library_commands
-      after_library_commands
       loaded_correctly? && (@loaded = true)
-    end
-
-    def load_module_commands?
-      @module
     end
 
     # Load the source and set instance variables necessary to make a library
@@ -29,38 +24,19 @@ module Boson
     # Callbacks for @module before loading
     def module_callbacks; end
 
-    def before_library_commands; end
-
-    def after_library_commands; end
-
-    # Boolean which indicates if library loaded correctly.
-    def loaded_correctly?
-      !!@module
+    # Determines if @module initializing happens
+    def load_module_commands?
+      @module
     end
 
-    # Callback for @module after it's been included
-    def after_include
-    end
-
-    #:stopdoc:
-
-    def load_module_commands
-      initialize_library_module
-    rescue MethodConflictError => err
-      handle_method_conflict_error err
-    end
-
-    def handle_method_conflict_error(err)
-      raise MethodConflictError, err.message
-    end
-
+    # Wraps around module loading for unexpected additions
     def detect_additions(options={}, &block)
-      options[:object_methods] = @object_methods if !@object_methods.nil?
-      detected = Util.detect(options, &block)
-      @commands += detected[:methods].map {|e| e.to_s }
-      detected
+      Util.detect(options, &block).tap do |detected|
+        @commands.concat detected[:methods].map(&:to_s)
+      end
     end
 
+    # loads module's commands
     def initialize_library_module
       @module = @module ? Util.constantize(@module) :
         Util.create_module(Boson::Commands, clean_name)
@@ -68,15 +44,50 @@ module Boson
       during_initialize_library_module
       check_for_method_conflicts unless @force
       after_initialize_library_module
+    rescue MethodConflictError => err
+      handle_method_conflict_error err
     end
 
-    def during_initialize_library_module
+    # Boolean which indicates if library loaded correctly.
+    def loaded_correctly?
+      !!@module
     end
 
+    # Callback for @module after it's been included
+    def after_include; end
+
+    def handle_method_conflict_error(err)
+      raise MethodConflictError, err.message
+    end
+
+    # Called after @module has been created
+    def during_initialize_library_module; end
+
+    # Actually includes module and its commands
     def after_initialize_library_module
       include_in_universe
     end
 
+    # Returns array of method conflicts
+    def method_conflicts
+      (@module.instance_methods + @module.private_instance_methods) &
+        (Boson.main_object.methods + Boson.main_object.private_methods)
+    end
+
+    # Handles setting and cleaning @commands
+    def set_library_commands
+      clean_library_commands
+    end
+
+    # Cleans @commands from set_library_commands
+    def clean_library_commands
+      aliases = @commands_hash.select {|k,v| @commands.include?(k) }.
+        map {|k,v| v[:alias]}.compact
+      @commands -= aliases
+      @commands.uniq!
+    end
+
+    private
     def include_in_universe(lib_module=@module)
       Boson::Universe.send :include, lib_module
       after_include
@@ -86,24 +97,9 @@ module Boson
     def check_for_method_conflicts
       conflicts = method_conflicts
       unless conflicts.empty?
-        raise MethodConflictError,"The following commands conflict with existing commands: #{conflicts.join(', ')}"
+        raise MethodConflictError, "The following commands conflict with " +
+          "existing commands: #{conflicts.join(', ')}"
       end
     end
-
-    def method_conflicts
-      (@module.instance_methods + @module.private_instance_methods) &
-        (Boson.main_object.methods + Boson.main_object.private_methods)
-    end
-
-    def set_library_commands
-      aliases = @commands_hash.select {|k,v| @commands.include?(k) }.map {|k,v| v[:alias]}.compact
-      @commands -= aliases
-      clean_library_commands
-      @commands.uniq!
-    end
-
-    def clean_library_commands
-    end
-    #:startdoc:
   end
 end
